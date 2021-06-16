d324343993bab6bb25bc9a3fc737d83e10cc6ae5
Roberto Rolo <robertorolo@gmail.com>
2021-06-16 11:39:48 -0300

    Add files via upload

--- balanco_hidrico.py
+++ balanco_hidrico.py
@@ -4,17 +4,44 @@ from tkinter import Tk, Label, Button, Entry
 from tkinter.filedialog import askopenfilename
 import geopandas
 from shapely.geometry import Point
+from shapely.ops import cascaded_union
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
 
-arquivo_bacias = "dados/Bacia_Hidrografica/Bacia_Hidrografica.shp"
+arquivo_bacias = "dados/bacias/Bacia_Hidrografica.shp"
 
 dic_cod_bacia = {
-    'gravataí': "dados/G010/G010_mini_19_02.shp",
+    'gravataí': "dados/mini_bacias/G010_mini_19_02.shp",
+    'sinos': "dados/mini_bacias/G020_mini_19_02.shp",
+    'caí': "dados/mini_bacias/G030_mini_19_02.shp",
+    'taquari-antas': "dados/mini_bacias/G040_mini_19_02.shp",
+    'alto jacuí': "dados/mini_bacias/G050_mini_19_02.shp",
+    'vacacaí-vacacaí-mirim': "dados/mini_bacias/G060_mini_19_02.shp",
+    'baixo jacuí': "dados/mini_bacias/G070_mini_19_02.shp",
+    'lago guaíba': "dados/mini_bacias/G080_mini_19_02.shp",
+    'pardo': "dados/mini_bacias/G090_mini_19_02.shp",
+    'tramandaí': "dados/mini_bacias/L010_mini_19_02.shp",
+    'litoral médio': "dados/mini_bacias/L020_mini_19_02.shp",
+    'camaquã': "dados/mini_bacias/L030_mini_19_02.shp",
+    'mirim-são gonçalo': "dados/mini_bacias/L040_mini_19_02.shp",
+    'mampituba': "dados/mini_bacias/L050_mini_19_02.shp",
+    'apuaê-inhandava': "dados/mini_bacias/U010_mini_19_02.shp",
+    'passo fundo': "dados/mini_bacias/U020_mini_19_02.shp",
+    'turvo santa rosa santo cristo': "dados/mini_bacias/U030_mini_19_02.shp",
+    'piratinim': "dados/mini_bacias/U040_mini_19_02.shp",
+    'ibicuí': "dados/mini_bacias/U050_mini_19_02.shp",
+    'quaraí': "dados/mini_bacias/U060_mini_19_02.shp",
+    'santa maria': "dados/mini_bacias/U070_mini_19_02.shp",
+    'negro': "dados/mini_bacias/U080_mini_19_02.shp",
+    'ijuí': "dados/mini_bacias/U090_mini_19_02.shp",
+    'várzea': "dados/mini_bacias/U100_mini_19_02.shp",
+    'butuí-icamaquã': "dados/mini_bacias/U110_mini_19_02.shp",
 }
 
 print('Essa janela de terminal é aberta para mostrar possíveis erros.\n')
+print('Envie qualquer mensagem de erro encontrada para roberto-rolo@sema.rs.gov.br.\n')
 
 def str_num_to_float(str_array):
     float_array = []
@@ -31,7 +58,7 @@ def calcular():
     
     print('Lendo arquivo das bacias hidrográficas...\n')
     bacias = geopandas.read_file(arquivo_bacias)
-    bacias = bacias.to_crs("EPSG:4674")
+    #bacias = bacias.to_crs("EPSG:4674")
     
     print('Lendo coordenadas...')
     longitude =  float(entry_x.get().replace(',','.'))
@@ -49,7 +76,7 @@ def calcular():
 
     print('Lendo arquivo de mini bacias...\n')
     mini_bacias = geopandas.read_file(dic_cod_bacia[bacia.lower()])
-    mini_bacias = mini_bacias.to_crs("EPSG:4674")
+    #mini_bacias = mini_bacias.to_crs("EPSG:4674")
 
     
     print('Verificando a qual mini bacia o ponto informado petence...')
@@ -61,42 +88,56 @@ def calcular():
             break
 
     print('Encontrando as mini bacias que pertencem a área de drenagem...')
+    print('Há {} mini bacias.'.format(len(mini_bacias)))
+    t1 = time()
     ad = [mini_bacia]
     ad_idx = [mini_bacia_idxs]
     ad_c = ad.copy()
+    c = 1
     
     existe_bacia = True
     while existe_bacia == True:
+        print('Iteração {} para {} mini bacias'.format(c, len(ad_c)))
         ad_p = []
         for mb in ad_c:
-            for idx, row in mini_bacias.iterrows():
-                if row['MiniJus'] == mb:
-                    ad_p.append(int(row['Mini']))
-                    ad_idx.append(idx)
+            f = mini_bacias['MiniJus'] == mb
+            filtrado = mini_bacias[f]
+            ad_p = ad_p + list(filtrado['Mini'].values)
+            ad_idx = ad_idx + list(filtrado.index.values)
         if len(ad_p) == 0:
             existe_bacia = False
         else:
             ad = ad + ad_p
             ad_c = ad_p.copy()
+            c = c + 1
 
     #removendo a minibacia original
     bd_i = ad_idx[0]
     ad = ad[1:]
     ad_idx = ad_idx[1:]
     
-    print('As mini bacias que pertencem a area de drenagem são {}\n'.format(ad))
+    t2 = time()
+    delta_t = t2 - t1
+    print('Isso levou {} segundos \n'.format(int(delta_t)))
+    
+    #print('As mini bacias que pertencem a area de drenagem são {}\n'.format(ad))
 
-    print('Encontrando os processos do SIOUT que pertencem a área de drenagem...\n')
+    print('Encontrando os processos do SIOUT que pertencem a área de drenagem...')
+    t1 = time()
+    polys = [mini_bacias.iloc[idx]['geometry'] for idx in ad_idx]
+    mini_bacias_uniao = geopandas.GeoSeries(cascaded_union(polys))
     xs = []
     ys = []
     ids = []
     for idx, row in df_extrato_siout.iterrows():
         p = Point(row['Longitude'], row['Latitude'])
-        for b_idx in ad_idx: 
-            if p.within(mini_bacias.loc[b_idx, 'geometry']):
-                xs.append(p.x)
-                ys.append(p.y)
-                ids.append(idx)
+        if p.within(mini_bacias_uniao[0]):
+            xs.append(p.x)
+            ys.append(p.y)
+            ids.append(idx)
+    t2 = time()
+    delta_t = t2 - t1
+    print('Isso levou {} segundos \n'.format(int(delta_t)))
 
     #Plotando os mapas
     fig, ax = plt.subplots(figsize=(12,12))
@@ -116,36 +157,43 @@ def calcular():
     plt.tight_layout()
     plt.show()
 
-    print('Feche a janela do mapa para mostrtar o próximo gráfico.')
+    print('Feche a janela do mapa para mostrtar o próximo gráfico.\n')sa
 
     #plotando o balanço hídrico
-    vaz_bacias = mini_bacias.loc[ad_idx, ['Qref01', 'Qref02', 'Qref03', 'Qref04', 'Qref05', 'Qref06', 'Qref07', 'Qref08', 'Qref09', 'Qref10', 'Qref11', 'Qref12']]
+    vaz_simulada = float(entry_vs.get().replace(',','.'))
+    vaz_bacias = mini_bacias.loc[bd_i, ['Qref01', 'Qref02', 'Qref03', 'Qref04', 'Qref05', 'Qref06', 'Qref07', 'Qref08', 'Qref09', 'Qref10', 'Qref11', 'Qref12']].values
     vaz_siout = df_extrato_siout.loc[ids, ['Vazão janeiro', 'Vazão fevereiro', 'Vazão março', 'Vazão abril', 'Vazão maio', 'Vazão junho', 'Vazão julho', 'Vazão agosto', 'Vazão setembro', 'Vazão outubro', 'Vazão novembro', 'Vazão dezembro']]
 
-    vaz_bacias_mes = np.sum(vaz_bacias, axis=0).values
-    vaz_siout_mes = np.sum(vaz_siout, axis=0).values
+    perc_out =  mini_bacias.loc[bd_i, 'perc_out']
+    print('Máximo outorgável: {}'.format(perc_out))
+    vaz_max_out = vaz_bacias * perc_out
+    vaz_siout_mes = np.sum(vaz_siout, axis=0).values 
+    bal_inicial = vaz_max_out - vaz_siout_mes 
+    bal_final = bal_inicial - vaz_simulada
+    
+    print('Vazão da bacia: {}'.format(vaz_bacias))
+    print('Usos SIOUT: {}'.format(vaz_siout_mes))
+    print('Balanço inicial: {}'.format(bal_inicial))
+    print('Balanço final: {}'.format(bal_final))
 
-    area_de_drenagem = np.sum(mini_bacias.loc[ad_idx, 'AreaMini'])
+    area_de_drenagem = mini_bacias.loc[bd_i, 'AreaDren']
     print('Área de drenagem: {}'.format(area_de_drenagem))
+    print('Vazão de referência: {}'.format('N/D'))
 
     tick_label = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
 
-    if np.sum(vaz_siout_mes) == 0:
-        fig, ax = plt.subplots(figsize=(12,12))
+    fig, axs = plt.subplots(2, 1, figsize=(12,12))
 
-        ax.bar([x for x in range(1, 13)], vaz_bacias_mes, tick_label=tick_label)
-        ax.set_ylabel('Balanço inicial')
+    axs[0].bar([x for x in range(1, 13)], bal_inicial, tick_label=tick_label)
+    axs[0].set_title('Balanço inicial')
+    axs[0].set_ylabel('Vazão m³/s')
+    axs[0].grid()
+    axs[1].bar([x for x in range(1, 13)], bal_final, tick_label=tick_label)
+    axs[1].set_title('Balanço final')
+    axs[1].set_ylabel('Vazão m³/s')
+    axs[1].grid()
 
-        plt.show()
-    else:
-        fig, axs = plt.subplots(2, 1, figsize=(12,12))
-
-        axs[0].bar([x for x in range(1, 13)], vaz_bacias_mes, tick_label=tick_label)
-        axs[0].set_ylabel('Balanço inicial')
-        axs[1].bar([x for x in range(1, 13)], (vaz_bacias_mes-vaz_siout_mes), tick_label=tick_label)
-        axs[1].set_ylabel('Balanço final')
-
-        plt.show()
+    plt.show()
 
 def selecionar_extrato():
     print('Lendo extrato do SIOUT...')
@@ -176,10 +224,16 @@ entry_y.grid(row=0, column=1, sticky='E', padx=10, pady=10)
 entry_x = Entry(root, width=8)
 entry_x.grid(row=1, column=1, sticky='E', padx=10, pady=10)
 
+label_vs = Label(root, text="Vazão simulada:")
+label_vs.grid(row=2, column=0, sticky='W', padx=10, pady=10)
+
+entry_vs = Entry(root, width=8)
+entry_vs.grid(row=2, column=1, sticky='E', padx=10, pady=10)
+
 btn_extrato = Button(root, text="Selecione o extrato do SIOUT", command=selecionar_extrato)
-btn_extrato.grid(row=2, column=0, sticky='W', padx=10, pady=10)
+btn_extrato.grid(row=3, column=0, sticky='W', padx=10, pady=10)
 
 btn_calcular = Button(root, text="Calcular balanço hídrico", command=calcular)
-btn_calcular.grid(row=3, column=1, sticky='E', padx=10, pady=10)
+btn_calcular.grid(row=4, column=1, sticky='E', padx=10, pady=10)
 
 root.mainloop()
